13e1028e2e80e113eb68c9a2f770b570fe3afc2c
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2023-12-23 13:45:06 +0100

    add custom loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,32 @@ const DEFAULT_OPTIONS = {
   fontSize: 16,
 } as const satisfies EditorProps["options"];
 
+const Loading: React.FC<React.SVGAttributes<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      className="h-10 w-10 animate-spin text-white"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <circle
+        cx={12}
+        cy={12}
+        r={10}
+        stroke="currentColor"
+        strokeWidth={4}
+        className="opacity-25"
+      />
+      <path
+        fill="currentColor"
+        d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        className="opacity-75"
+      />
+    </svg>
+  );
+};
+
 /**
  * @see https://github.com/suren-atoyan/monaco-react
  * @see https://github.com/typehero/typehero/blob/main/packages/monaco/src/code-editor.tsx
@@ -25,6 +51,7 @@ export default function Home() {
         className="border border-zinc-700"
         defaultLanguage="typescript"
         height="90vh"
+        loading={<Loading />}
         options={DEFAULT_OPTIONS}
         theme="vs-dark"
       />
